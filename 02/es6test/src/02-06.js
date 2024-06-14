34aed7f717bdef231a45e03ff0504018c37253ee
zerotymer <hyunjin.shin@outlook.com>
2024-06-14 17:02:28 +0900

    2.4 기본 파라미터와 가변 파라미터

---
+++ 02/es6test/src/02-06.js
@@ -0,0 +1,7 @@
+function foodReport(name, age, ...favoriteFoods) {
+    console.log(name + ', ' + age);
+    console.log(favoriteFoods);
+}
+
+foodReport('이몽룡', 20, '짜장명', '냉면', '불고기');
+foodReport('홍길동', 16, '초밥');
