7908b8321456078079bf0391111a32348a7b10b7
zerotymer <hyunjin.shin@outlook.com>
2024-06-14 16:42:17 +0900

    2.3 let과 const

---
+++ 02/es6test/build/02-02.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var msg = "GLOBAL";
+function outer() {
+  var msg = "OUTER";
+  console.log(msg);
+  if (true) {
+    var _msg = "BLOCK";
+    console.log(_msg);
+  }
+}
+outer();
+console.log(msg);
---
+++ 02/es6test/src/02-02.js
@@ -0,0 +1,15 @@
+let msg = "GLOBAL";
+
+function outer() {
+    let msg = "OUTER";
+    console.log(msg);
+
+    if (true) {
+        let msg = "BLOCK";
+        console.log(msg);
+    }
+
+}
+
+outer();
+console.log(msg);
