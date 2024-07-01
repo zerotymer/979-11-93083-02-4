541cdd1d6a5bc25f673bce120d46931adcd06d74
zerotymer <hyunjin.shin@outlook.com>
2024-07-01 16:46:56 +0900

    7.2.3 간단한 단일 파일 컴포넌트 작성과 사용

--- 07/vue/test-vue-app/src/App.vue
+++ 07/vue/test-vue-app/src/App.vue
@@ -1,47 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h2> App 컴포넌트</h2>
+    <hr />
+    <ul>
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />  
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import CheckboxItem from './components/CheckboxItem.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+export default {
+  name: 'App',
+  components: { CheckboxItem }
+};
+</script>
---
+++ 07/vue/test-vue-app/src/components/CheckboxItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <li>
+        <input type="checkbox" v-model="checked" /> 옵션
+    </li>
+</template>
+
+<script>
+    export default {
+        name: 'CheckboxItem',
+        data: () => ({
+            checked: false
+        })
+    }
+</script>
+
+<style scoped>
+
+</style>
