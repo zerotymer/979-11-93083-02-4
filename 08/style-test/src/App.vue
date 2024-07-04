fa20ebd6fdccb58b7eb932d4be7e2688b6f4644a
zerotymer <hyunjin.shin@outlook.com>
2024-07-04 20:59:56 +0900

    8.1 단일 파일 컴포넌트에서의 스타일

---
+++ 08/style-test/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Child1 from './components/Child1.vue';
+import Child2 from './components/Child2.vue';
+import Child3 from './components/Child3.vue';
+
+export default {
+    name: 'App',
+    components: { Child1, Child2, Child3 }
+}
+</script>
+
+
+<template>
+  <div>
+    <Child1 />
+    <Child2 />
+    <Child3 />
+  </div>
+</template>
---
+++ 08/style-test/src/components/Child3.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="child">
+        <h2>Child3</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Child3'
+}
+</script>
+
+<style>
+.child { background-color: orange; border: solid 1px black; margin: 1.5em; padding: 1.0em; }
+</style>
