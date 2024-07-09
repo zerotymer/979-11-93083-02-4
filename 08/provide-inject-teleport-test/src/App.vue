74d8bcbb3a46a51d76abf3c4e33542f95e12d8e6
zerotymer <hyunjin.shin@outlook.com>
2024-07-10 01:18:56 +0900

    8.6 텔레포트

--- 08/provide-inject-teleport-test/src/App.vue
+++ 08/provide-inject-teleport-test/src/App.vue
@@ -2,6 +2,12 @@
   <div>
     <h2>최신 인기곡</h2>
     <SongList :songs="songs" />
+    <br />
+    <br />
+    <button @click="changeModal">Teleport를 이용한 Modal 기능</button>
+    <teleport to="#modal">
+      <Modal v-if="isModal" />
+    </teleport>
   </div>
 </template>
 
@@ -9,17 +15,19 @@
 <script>
 import{ computed } from 'vue';
 import SongList from './components/SongList.vue';
+import Modal from './components/Modal.vue';
 
 export default {
   name: 'App',
-  components: { SongList },
+  components: { SongList, Modal },
   data: () => ({
     songs: [
       { id: 1, title: 'Blueming', done: true },
       { id: 2, title: 'Dynamite', done: true },
       { id: 3, title: 'Lovesick Girls', done: false },
       { id: 4, title: '마리아(Maria)', done: false }
-    ]
+    ],
+    isModal: false
   }),
   provide: function() {
     return {
@@ -27,9 +35,15 @@ export default {
         checked: 'far fa-check-circle',
         unchecked: 'far fa-circle'
       },
-      // doneCount: computed(() => (this.songs.filter(s => s.done === true).length ))
-      doneCount: this.songs.filter(s => s.done === true).length
+      doneCount: computed(() => (this.songs.filter(s => s.done === true).length ))
+      // doneCount: this.songs.filter(s => s.done === true).length
     };
+  },
+  methods: {
+    changeModal: function() {
+      this.isModal = true;
+      // setTimeout(() => { this.isModal = false}, 2000);
+    }
   }
 }
 </script>
