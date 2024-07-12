fc6c158fdb3d403994d911f2a113aa6cee146b1f
zerotymer <hyunjin.shin@outlook.com>
2024-07-12 12:27:57 +0900

    9.2 setup 메서드를 이용한 초기화

---
+++ 09/calc-component-test/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Calc />
+  </div>
+</template>
+
+
+<script>
+import Calc from './components/Calc.vue'
+
+export default {
+  name: 'App',
+  components: { Calc }  
+}
+</script>
