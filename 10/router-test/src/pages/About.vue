c38170875fac79e78b7408f826525cd86aae4029
zerotymer <hyunjin.shin@outlook.com>
2024-07-17 15:55:24 +0900

    10.3 rotuer 객체와 currentRoute 객체

--- 10/router-test/src/pages/About.vue
+++ 10/router-test/src/pages/About.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="card card-body">
         <h2>About</h2>
+        <p>요청 경로: {{ $route.fullPath }}</p>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'About'
+    name: 'About',
+    created: function() {
+        console.log(this.$route)
+    }
 }
 </script>
