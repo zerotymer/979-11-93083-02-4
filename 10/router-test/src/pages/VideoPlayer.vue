f54c269b18fff2b0f425a56ffe87a1bac9aacbb4
zerotymer <hyunjin.shin@outlook.com>
2024-07-22 19:21:23 +0900

    10.8.1 히스토리 모드

--- 10/router-test/src/pages/VideoPlayer.vue
+++ 10/router-test/src/pages/VideoPlayer.vue
@@ -79,7 +79,7 @@ export default {
 
 <style scoped>
 .modal { display: block; position: fixed; z-index: 1;
-    left: 0; top: 0; width: 100%; hight: 100%; 
+    left: 0; top: 0; width: 100%; height: 100%; 
     overflow: auto;
     background-color: rgba(0, 0, 0, 0.4)}
 .box { background-color: white; margin: 80px auto;
