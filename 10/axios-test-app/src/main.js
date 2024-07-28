975b6ac4ae411bbbc0d9de5ee9f5ef509022a429
zerotymer <hyunjin.shin@outlook.com>
2024-07-29 00:05:04 +0900

    11.6.2 axios 라이브러리 사용 방법

--- 10/axios-test-app/src/main.js
+++
@@ -1,6 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App2.vue'
-
-createApp(App).mount('#app')
---
+++ 11/axios-test-app/src/App4.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>콘솔을 확인합니다.</h2>
+    </div>
+</template>
+
+
+<script setup>
+import axios from 'axios'
+
+const requestAPI = async () => {
+    const url = '/api/todolist/gdhong'
+    let data = { todo: '윗몸일으키기 3세트', desc: '너무 빠르지 않게...' }
+    const response = await axios.post(url, data)
+    console.log('# 응답객체: ', response)
+}
+
+requestAPI()
+</script>
