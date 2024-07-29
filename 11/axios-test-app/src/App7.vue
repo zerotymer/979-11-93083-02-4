30a4c8162d6e3f72e331f7adffb199ed27572e52
zerotymer <hyunjin.shin@outlook.com>
2024-07-29 12:51:14 +0900

    11.6.3 에러 처리

---
+++ 11/axios-test-app/src/App7.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>콘솔을 확인합니다.</h2>
+    </div>
+</template>
+
+
+<script setup>
+import axios from 'axios'
+
+const requestAPI = async () => {
+    const url = '/api/todolist_long/gdhong'
+    const response = await axios.get(url, { timeout: 2200 })
+        .catch((e) => {
+            console.log('오류가 발생하였습니다.')
+            if (e instanceof Error)     console.log(e.message)
+            else                        console.log(e) 
+
+            return
+        })
+
+    // .then((response) => {
+    //         console.log('# 응답객체: ', response)
+    //     })
+        console.log('# 응답객체: ', response)
+}
+
+requestAPI()
+</script>
--- 11/axios-test-app/src/main.js
+++ 11/axios-test-app/src/main.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 // import App from './App.vue'
 // import App from './App2.vue'
 // import App from './App3.vue'
-import App from './App4.vue'
+// import App from './App4.vue'
+// import App from './App5.vue'
+import App from './App6.vue'
 
 createApp(App).mount('#app')
